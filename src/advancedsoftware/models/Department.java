ada54ca7b19f91fd5767838ef233193637be0ac2
bolaFayz <bolafayz3@gmail.com>
2026-05-08 16:11:13 +0300

    Department

--- src/advancedsoftware/models/Department.java
+++ src/advancedsoftware/models/Department.java
@@ -1,16 +1,15 @@
 package advancedsoftware.models;
 
 public class Department {
-    private int departmentId;
-    private String deptName;
-    private String description;
-    private int managerId;
+    public int department_id;
+    public String dept_name;
+    public String description;
+    public int manager_id;
 
-    public Department(int departmentId, String deptName, String description, int managerId) {
-        this.departmentId = departmentId;
-        this.deptName = deptName;
-        this.description = description;
-        this.managerId = managerId;
+    public Department(int id, String name, String desc, int manager) {
+        this.department_id = id;
+        this.dept_name = name;
+        this.description = desc;
+        this.manager_id = manager;
     }
-
 }
